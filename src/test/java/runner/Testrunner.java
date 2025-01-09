1920097a756d32b54d309dc8b14fa9cdbf6bfea4
SarathSobhitharaj <sarathsobhitharaj@gmail.com>
2025-01-09 12:57:14 +0530

    please commit

---
+++ src/test/java/runner/Testrunner.java
@@ -0,0 +1,25 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+@CucumberOptions(features ={"src/test/resources/Features/DataGeneration.feature"},
+        tags ="@b",
+        glue = {"com.framium.stepdef"})
+
+@RunWith(Cucumber.class)
+public class Testrunner{
+@BeforeClass
+    public void beforeAll(){
+
+}
+
+@AfterClass
+    public void AfterAll(){
+
+}
+
+}
